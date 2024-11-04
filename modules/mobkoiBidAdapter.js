cd2bb3bb67fed5dab03bbd0d99e9e99b477a9025
zhihuiye <zhihui@mobkoi.com>
2024-11-04 14:29:08 +0000

    solved invalid category issue

--- modules/mobkoiBidAdapter.js
+++ modules/mobkoiBidAdapter.js
@@ -12,49 +12,33 @@ const converter = ortbConverter({
     netRevenue: true,
     ttl: 30,
   },
-  imp(buildImp, bidRequest, context) {
-    const imp = buildImp(bidRequest, context);
-    if (!imp.bidfloor) {
-      imp.bidfloor = bidRequest.params.bidfloor || 0;
-      imp.bidfloorcur = bidRequest.params.currency || DEFAULT_CURRENCY;
-    }
-    if (bidRequest.params.battr) {
-      Object.keys(bidRequest.mediaTypes).forEach(mType => {
-        imp[mType].battr = bidRequest.params.battr;
-      })
-    }
-    return imp;
-  },
-  request(buildRequest, imps, bidderRequest, context) {
-    const request = buildRequest(imps, bidderRequest, context);
-    const bid = context.bidRequests[0];
-    if (!request.cur) {
-      request.cur = [bid.params.currency || DEFAULT_CURRENCY];
-    }
-    if (bid.params.bcat) {
-      request.bcat = bid.params.bcat;
-    }
-    return request;
-  },
+  // imp(buildImp, bidRequest, context) {
+  //   const imp = buildImp(bidRequest, context);
+  //   if (!imp.bidfloor) {
+  //     imp.bidfloor = bidRequest.params.bidfloor || 0;
+  //     imp.bidfloorcur = bidRequest.params.currency || DEFAULT_CURRENCY;
+  //   }
+  //   if (bidRequest.params.battr) {
+  //     Object.keys(bidRequest.mediaTypes).forEach(mType => {
+  //       imp[mType].battr = bidRequest.params.battr;
+  //     })
+  //   }
+  //   return imp;
+  // },
+  // request(buildRequest, imps, bidderRequest, context) {
+  //   const request = buildRequest(imps, bidderRequest, context);
+  //   const bid = context.bidRequests[0];
+  //   if (!request.cur) {
+  //     request.cur = [bid.params.currency || DEFAULT_CURRENCY];
+  //   }
+  //   if (bid.params.bcat) {
+  //     request.bcat = bid.params.bcat;
+  //   }
+  //   return request;
+  // },
   bidResponse(buildBidResponse, bid, context) {
-    const { bidRequest } = context;
-
-    let resMediaType;
-    const reqMediaTypes = Object.keys(bidRequest.mediaTypes);
-    if (reqMediaTypes.length === 1) {
-      resMediaType = reqMediaTypes[0];
-    } else {
-      if (bid.adm.search(/^(<\?xml|<vast)/i) !== -1) {
-        resMediaType = VIDEO;
-      } else if (bid.adm[0] === '{') {
-        resMediaType = NATIVE;
-      } else {
-        resMediaType = BANNER;
-      }
-    }
-
-    context.mediaType = resMediaType;
-    context.cpm = bid.price;
+    // eslint-disable-next-line no-console
+    console.log({ context, bid });
 
     const bidResponse = buildBidResponse(bid, context);
     return bidResponse;
@@ -89,8 +73,8 @@ export const spec = {
 
     const responseBody = {...serverResponse.body, seatbid: serverResponse.body.seatbid};
     const prebidBidResponse = converter.fromORTB({
-      response: responseBody,
       request: bidRequest.data.ortb,
+      response: responseBody,
     });
 
     console.log({ serverResponse, bidRequest, prebidBidResponse });
