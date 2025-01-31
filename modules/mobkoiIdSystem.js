87e5bf2d99c5e572a3b8e2fa634ce59253de781c
zhihuiye <zhihui@mobkoi.com>
2025-01-31 15:21:00 +0000

    added unit tests for mobkoi ID system module

--- modules/mobkoiIdSystem.js
+++ modules/mobkoiIdSystem.js
@@ -11,8 +11,10 @@ import { logError, logInfo, deepAccess, insertUserSyncIframe } from '../src/util
 
 const GVL_ID = 898;
 const MODULE_NAME = 'mobkoiId';
-const PROD_AD_SERVER_BASE_URL = 'https://adserver.maximus.mobkoi.com';
-const COOKIE_KEY_EQUATIV_SAS_ID = '__mobkoi_sas_id';
+export const PROD_AD_SERVER_BASE_URL = 'https://adserver.maximus.mobkoi.com';
+export const COOKIE_KEY_EQUATIV_SAS_ID = '__mobkoi_sas_id';
+export const EQUATIV_BASE_URL = 'https://sync.smartadserver.com';
+export const EQUATIV_NETWORK_ID = '5290';
 /**
  * !IMPORTANT: This value must match the value in mobkoiAnalyticsAdapter.js
  * The name of the parameter that the publisher can use to specify the ad server endpoint.
@@ -50,25 +52,24 @@ export const mobkoiIdSubmodule = {
     logInfo(`Cannot found "${COOKIE_KEY_EQUATIV_SAS_ID}" in local cookie with name.`);
     return {
       callback: () => {
-        requestEquativSasId(
-          userSyncOptions,
-          gdprConsent,
-          (sasId) => {
-            if (!sasId) {
-              logError('Equativ SAS ID is empty');
-              return {
-                id: null
+        return new Promise((resolve, _reject) => {
+          utils.requestEquativSasId(
+            userSyncOptions,
+            gdprConsent,
+            (sasId) => {
+              if (!sasId) {
+                logError('Equativ SAS ID is empty');
+                resolve({ id: null });
+                return;
               }
-            }
 
-            logInfo(`Fetched Equativ SAS ID: "${sasId}"`);
-            storage.setCookie(COOKIE_KEY_EQUATIV_SAS_ID, sasId);
-            logInfo(`Stored Equativ SAS ID in local cookie with name: "${COOKIE_KEY_EQUATIV_SAS_ID}"`);
-            return {
-              id: sasId
+              logInfo(`Fetched Equativ SAS ID: "${sasId}"`);
+              storage.setCookie(COOKIE_KEY_EQUATIV_SAS_ID, sasId);
+              logInfo(`Stored Equativ SAS ID in local cookie with name: "${COOKIE_KEY_EQUATIV_SAS_ID}"`);
+              resolve({ id: sasId });
             }
-          }
-        );
+          );
+        });
       }
     };
   },
@@ -76,49 +77,63 @@ export const mobkoiIdSubmodule = {
 
 submodule('userId', mobkoiIdSubmodule);
 
-function requestEquativSasId(syncUserOptions, gdprConsent, onCompleteCallback) {
-  logInfo('Start requesting Equativ SAS ID');
+export const utils = {
+  requestEquativSasId(syncUserOptions, gdprConsent, onCompleteCallback) {
+    logInfo('Start requesting Equativ SAS ID');
+    const adServerBaseUrl = new URL(
+      deepAccess(
+        syncUserOptions,
+        `params.${PARAM_NAME_AD_SERVER_BASE_URL}`) || PROD_AD_SERVER_BASE_URL
+    );
 
-  const equativPixelUrl = buildEquativPixelUrl(syncUserOptions, gdprConsent);
-  logInfo('Equativ SAS ID request URL:', equativPixelUrl);
+    const equativPixelUrl = utils.buildEquativPixelUrl(syncUserOptions, gdprConsent);
+    logInfo('Equativ SAS ID request URL:', equativPixelUrl);
 
-  const url = 'https://adserver.local.mobkoi.com/pixeliframe?' +
-    'pixelUrl=' + encodeURIComponent(equativPixelUrl) +
-    '&cookieName=sas_uid';
+    const url = adServerBaseUrl + '/pixeliframe?' +
+      'pixelUrl=' + encodeURIComponent(equativPixelUrl) +
+      '&cookieName=sas_uid';
+
+    /**
+     * Listen for messages from the iframe
+     */
+    window.addEventListener('message', function(event) {
+      switch (event.data.type) {
+        case 'MOBKOI_PIXEL_SYNC_COMPLETE':
+          const sasUid = event.data.syncData;
+          logInfo('Parent window Sync completed. SAS ID:', sasUid);
+          onCompleteCallback(sasUid);
+          break;
+        case 'MOBKOI_PIXEL_SYNC_ERROR':
+          logError('Parent window Sync failed:', event.data.error);
+          break;
+      }
+    });
+
+    insertUserSyncIframe(url, () => {
+      logInfo('insertUserSyncIframe loaded');
+    });
+
+    // Return the URL for testing purposes
+    return url;
+  },
 
   /**
-   * Listen for messages from the iframe
+   * Build a pixel URL that will be placed in an iframe to fetch the Equativ SAS ID
    */
-  window.addEventListener('message', function(event) {
-    switch (event.data.type) {
-      case 'MOBKOI_PIXEL_SYNC_COMPLETE':
-        const sasUid = event.data.syncData;
-        logInfo('Parent window Sync completed. SAS ID:', sasUid);
-        onCompleteCallback(sasUid);
-        break;
-      case 'MOBKOI_PIXEL_SYNC_ERROR':
-        logError('Parent window Sync failed:', event.data.error);
-        break;
-    }
-  });
+  buildEquativPixelUrl(syncUserOptions, gdprConsent) {
+    logInfo('Generating Equativ SAS ID request URL');
+    const adServerBaseUrl = new URL(
+      deepAccess(
+        syncUserOptions,
+        `params.${PARAM_NAME_AD_SERVER_BASE_URL}`) || PROD_AD_SERVER_BASE_URL
+    );
 
-  insertUserSyncIframe(url, () => {
-    logInfo('insertUserSyncIframe loaded');
-  });
-}
+    const gdprConsentString = gdprConsent && gdprConsent.gdprApplies ? gdprConsent.consentString : '';
+    const smartServerUrl = EQUATIV_BASE_URL + '/getuid?' +
+      `url=` + encodeURIComponent(`${adServerBaseUrl}getPixel?value=`) + '[sas_uid]' +
+      `&gdpr_consent=${gdprConsentString}` +
+      `&nwid=${EQUATIV_NETWORK_ID}`;
 
-/**
- * Build a pixel URL that will be placed in an iframe to fetch the Equativ SAS ID
- */
-function buildEquativPixelUrl(syncUserOptions, gdprConsent) {
-  logInfo('Generating Equativ SAS ID request URL');
-  const adServerBaseUrl = new URL(deepAccess(syncUserOptions, `params.${PARAM_NAME_AD_SERVER_BASE_URL}`) || PROD_AD_SERVER_BASE_URL);
-
-  const gdprConsentString = gdprConsent && gdprConsent.gdprApplies ? gdprConsent.consentString : null;
-  const smartServerUrl = 'https://sync.smartadserver.com/getuid?' +
-    `url=` + encodeURIComponent(`${adServerBaseUrl}getPixel?value=`) + '[sas_uid]' +
-    `&gdpr_consent=${gdprConsentString}` +
-    `&nwid=5290`;
-
-  return smartServerUrl;
-}
+    return smartServerUrl;
+  }
+};
